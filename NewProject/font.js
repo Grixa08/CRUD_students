f1a52b5e7171c3b7471e58707486258562c40295
Grixa08 <brazhnik-03@list.ru>
2024-03-31 19:20:39 +0700

    feat: Сделал фильтрацию

--- NewProject/font.js
+++ NewProject/font.js
@@ -1,4 +1,4 @@
-let name = document.getElementById("i")
+let name1 = document.getElementById("i")
 let surname = document.getElementById("f")
 let lastname = document.getElementById("o")
 let birthday = document.getElementById("br")
@@ -9,11 +9,16 @@ let table = document.getElementById("table")
 
 let button1 = document.getElementById("add");
 
+let fiofilt = document.getElementById("fiofilt")
+let facfilt = document.getElementById("facfilt")
+let filtYear1 = document.getElementById("filtYear1")
+let filtYear2 = document.getElementById("filtYear2")
+
 button1.addEventListener('click', async () => {
 try {
   const response = await fetch('http://localhost:3000/api/students', {
     method: 'post',
-    body: JSON.stringify({name: name.value, surname: surname.value, lastname: lastname.value, birthday: birthday.value, studyStart: year2.value, faculty: faculty.value}),
+    body: JSON.stringify({name: name1.value, surname: surname.value, lastname: lastname.value, birthday: birthday.value, studyStart: year2.value, faculty: faculty.value}),
     headers: { 'Content-Type': 'application/json' }
   }); 
   if (response.ok) {
@@ -43,7 +48,7 @@ deleteButtons.forEach(button => {
   });
 });
 button1.innerHTML = "Добавить студента";
-name.style.border = "none";
+name1.style.border = "none";
 surname.style.border = "none";
 lastname.style.border = "none";
 birthday.style.border = "none";
@@ -55,8 +60,8 @@ faculty.style.border = "none";
 } catch (error) {
   console.error(error);
   button1.innerHTML = "Произошла ошибка";
-  if (name.value === "") {name.style.border = "1px solid red";}
-    else{name.style.border = "none";}
+  if (name1.value === "") {name1.style.border = "1px solid red";}
+    else{name1.style.border = "none";}
   if (surname.value === "") {surname.style.border = "1px solid red";}
     else{surname.style.border = "none";}
   if (lastname.value === "") {lastname.style.border = "1px solid red";}
@@ -69,3 +74,31 @@ faculty.style.border = "none";
     else{faculty.style.border = "none";}
   }
 });
+
+function filter(){
+  const filt1 = fiofilt.value.toLowerCase();
+  const filt2 = facfilt.value.toLowerCase();
+  const filt3 = filtYear1.value;
+  const filt4 = filtYear2.value;
+  for(let i = 1; i < table.rows.length; i++){
+    const row = table.rows[i]
+    const name = row.cells[0].textContent.toLowerCase();
+    const facultyfilt = row.cells[1].textContent.toLowerCase();
+    const startyear = row.cells[2].textContent;
+    const endyear = row.cells[3].textContent;
+
+    const isNameMatch = name.includes(filt1);
+    const isFacultyMatch = facultyfilt.includes(filt2);
+    const isStartYearMatch = startyear.includes(filt3);
+    const isEndYearMatch = endyear.includes(filt4);
+  if (isNameMatch && isFacultyMatch && isStartYearMatch && isEndYearMatch){
+    row.style.display = '';
+  } else{
+      row.style.display = "none";
+    }
+  }
+}
+fiofilt.addEventListener('input', filter);
+facfilt.addEventListener('input', filter);
+filtYear1.addEventListener('input', filter);
+filtYear2.addEventListener('input', filter);
